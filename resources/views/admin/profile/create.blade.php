fcf6ce4668c916475e86a148c35b1723257620a1
nobuya <anobu4@gmail.com>
2020-11-02 13:32:14 +0000

    念のため更新20

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible"
-        content = "IE=edge">
-        <meta name="viewport" content="width=device-width",
-        "initial-scale=1">
-        <title>Myprofile</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyProfile</title>
     </head>
-<body>
-    <h1>MyProfile画面</h1>
-    {{ --layouts/profile.blade.phpを読み込む --}}
-    @extends('layouts.profile')
-    
+    <body>
+        <h1>Myプロフィール画面</h1>
+        {{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- admin.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
+@section('title', 'My プロフィール')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
     </body>
-    
-        
 </html>
