210b66d3c35d80f6d4a74587b91d77d687e63e85
nobuya <anobu4@gmail.com>
2020-11-04 07:25:11 +0000

    formの調整

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,5 +1,7 @@
 
+@extends('layouts.admin');
 @section('content')
+
     <div class="container">
                          <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
 
@@ -27,19 +29,22 @@
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="hobby">
                         </div>
+                    </div>
+
                      <div class="form-group row">
                         <label class="col-md-2">自己紹介(introduction)</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
                         </div>
-                    </div>
-                    {{ csrf_field() }}
+                     </div>
+                     <div class="form-group row">
+                                            {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="送信">
+                    </div>
                 </form>
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <!--<h2>My プロフィール</h2>-->
             </div>
         </div>
-    </div>
 @endsection
